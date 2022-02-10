145255538c18535c8f069e14d493ddf1c39949b9
codepanther <pezechigozirim@gmail.com>
2022-02-10 13:39:21 +0100

    Update and delete post functionality

--- routes/posts.js
+++ routes/posts.js
@@ -14,4 +14,34 @@ router.post('/', async (req, res) => {
   }
 });
 
+// update a post
+router.put('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (req.body.userId !== post.userId)
+      res.status(403).json('You are not the owner of this post.');
+    else {
+      await post.updateOne({ $set: req.body })
+      res.status(200).json('Updated successfully.');
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// delete a post
+router.delete('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (req.body.userId !== post.userId)
+      res.status(403).json('You are not the owner of this post.');
+    else {
+      await post.deleteOne();
+      res.status(200).json('Post deleted successfully.');
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
 module.exports = router;
