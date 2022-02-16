e30fb5a1f9869fae41ac26ce0022844caeeb7ae6
codepanther <pezechigozirim@gmail.com>
2022-02-16 11:06:16 +0100

    Get all loggedInUser's posts

--- routes/posts.js
+++ routes/posts.js
@@ -74,6 +74,17 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+// get all loggedInUser's posts
+router.get('/:username/all', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.params.username });
+    const posts = await Post.find({ userId: user._id });
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // get posts on timeline - user posts + followed users' posts
 router.get('/timeline/:userId', async (req, res) => {
   try {
