d3fb7f83a41cef6a420575b75fa3aadcd8567a7a
codepanther <pezechigozirim@gmail.com>
2022-02-20 12:12:38 +0100

    Hide pw and updatedAt in successful login response

--- routes/auth.js
+++ routes/auth.js
@@ -14,20 +14,24 @@ router.post('/register', async (req, res) => {
   } catch (err) {
     res.status(500).json(err)
   }
-  
+
 });
 
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const user = await User.findOne({ email: email });
+    const user = await User.findOne({ email });
     !user && res.status(400).json('Sorry, user does not exist!');
 
     const correctPassword = await bcrypt.compare(password, user.password);
     !correctPassword && res.status(400).json('Incorrect password');
 
     // if login successful
+    // return the user without password and updatedAt
+    delete user._doc.password
+    delete user._doc.updatedAt
+
     res.status(200).json(user);
   } catch (err) {
     res.status(500).json(err);
